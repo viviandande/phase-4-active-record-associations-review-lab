1ebb977a558ada129e597cb211172010fcec2bd3
+ <+>
2023-03-21 14:17:50 +0300

    Completed assignment

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,7 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,6 +1,7 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
+      t.string :name
 
       t.timestamps
     end
